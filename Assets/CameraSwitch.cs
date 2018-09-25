25ae73b8f6296afc40d4ad90191bcad57071046e
Druffel12 <Druffel12@gmail.com>
2018-09-24 17:10:26 -0700

    Finished Effects
    effects finished and built

--- Assets/CameraSwitch.cs
+++ Assets/CameraSwitch.cs
@@ -6,20 +6,15 @@ using Cinemachine;
 public class CameraSwitch : MonoBehaviour {
 
     public CinemachineClearShot cam1;
-   
 
-	// Use this for initialization
-	void Start ()
+
+    // Use this for initialization
+    void Start()
     {
-        if (Input.GetKeyDown ("E"))
-        {
-            cam1.Priority -= 1;
-        }
-        else if (Input.GetKeyDown("Q"))
-        {
-            cam1.Priority += 1;
-        }
-	}
+
+        cam1.Priority -= 1;
+
+    }
 	
 	// Update is called once per frame
 	void Update ()
---
+++ Assets/LaserStart.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserStart : MonoBehaviour
+{
+    public Animator anim;
+    public GameObject LaserEffects;
+	
+    public void Laser()
+    {
+        anim.SetTrigger("ButtonPushed");
+        GameObject Lazar = Instantiate(LaserEffects);
+        Destroy(Lazar, 6f);
+        
+    }
+    //IEnumerator EndLaser(float delay)
+    //{
+    //    yield return new WaitForSeconds(delay);
+    //    Destroy(Lazar);
+    //    StopAllCoroutines();
+    //    yield break;
+        
+    //}
+    ////void End()
+    ////{
+    ////    StopCoroutine(EndLaser(0.5f));
+    ////}
+}
